6770ebb8f02811ae0b4466c16c6634f7f6f55db2
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-10-12 02:24:51 +0200

    State Flow Used Commit

--- app/src/main/java/com/example/simplestateflow/MainActivity.kt
+++ app/src/main/java/com/example/simplestateflow/MainActivity.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tvNum : TextView
+    private lateinit var tvNum: TextView
     private lateinit var timerViewModel: TimerViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +20,11 @@ class MainActivity : AppCompatActivity() {
 
         timerViewModel.startTimer()
 
-        timerViewModel.timerLiveData.observe(this) { num ->
-            println("Number : $num")
-            tvNum.text = num.toString()
+        lifecycleScope.launch {
+            timerViewModel.timerStateFlow.collect {
+                println("Number : $it")
+                tvNum.text = it.toString()
+            }
         }
     }
 }
--- app/src/main/java/com/example/simplestateflow/TimerViewModel.kt
+++ app/src/main/java/com/example/simplestateflow/TimerViewModel.kt
@@ -5,16 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class TimerViewModel : ViewModel() {
-    private val _timerLiveData = MutableLiveData<Int>()
-    val timerLiveData = _timerLiveData as LiveData<Int>
+//    private val _timerLiveData = MutableLiveData<Int>()
+//    val timerLiveData = _timerLiveData as LiveData<Int>
+
+    private val _timerStateFlow = MutableStateFlow(0)
+    val timerStateFlow = _timerStateFlow as StateFlow<Int>
 
     fun startTimer(){
         viewModelScope.launch{
             for (i in 1..20){
-                _timerLiveData.value = i
+                _timerStateFlow.emit(i)
                 delay(1000L)
             }
         }
